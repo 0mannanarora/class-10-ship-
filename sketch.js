c536cf1ca20568ead690535341aa17830d026a5d
0mannanarora <87897643+0mannanarora@users.noreply.github.com>
2021-07-30 19:20:28 +0930

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var sea
+var sea_1 
+var ships
+var ships_1
+
+
+function preload()
+{
+ sea_1 = loadImage("sea.png");
+
+ ships_1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png");
+}
+
+function setup(){
+  createCanvas(300,300);
+  sea = createSprite(150,150,300,300); 
+  sea.velocityX = -5
+  ships = createSprite(250,200,50,50);
+  sea.addImage(sea_1) ;
+  sea.scale = 0.2
+  
+  
+
+  
+  ships.addAnimation("cruise",ships_1);
+  ships.scale = 0.2
+  
+}
+
+function draw() {
+  background("blue");
+
+  if (sea.x < 0) 
+  {
+    sea.x = 150
+  }
+  
+   
+
+
+
+
+
+  drawSprites()
+}
